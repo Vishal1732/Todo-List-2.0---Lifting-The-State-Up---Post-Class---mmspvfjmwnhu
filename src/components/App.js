b82c9ae5fbc4c4abcd3fa551ec30849f09f5d172
Vishal1732 <92910196+Vishal1732@users.noreply.github.com>
2023-02-23 22:58:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,106 @@
-import React, { useState } from 'react'
-import SideNav from './SideNav';
-import MainSection from './MainSection';
-import Header from './Header';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../styles/App.css';
-const App = () => {
+import "./styles.css";
+import { useState } from "react";
 
-
+function TodoInput(props) {
+  const { onSubmit } = props;
   return (
-    <div id="main">
-    <Header />
-      <div className='row'>
-        <div className='col-md-2'>
-          <SideNav change={}/>
-        </div>
-        <div className='col-md-10'>
-          <MainSection active = {}/>
-        </div>
-      </div>
-    </div>
-  )
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        const value = event.target["todo-input"].value;
+
+        onSubmit(value);
+      }}
+    >
+      <input
+        style={{ width: "250px", padding: "5px" }}
+        type="text"
+        name="todo-input"
+      />
+    </form>
+  );
 }
 
+function TodoItem(props) {
+  // const items = props.items;
+  const { items, removeItem, toggleDone } = props;
 
-export default App;
+  return (
+    <div className="todo-item-container">
+      {items.map((el) => {
+        return (
+          <div className="todo-item">
+            <input
+              type="checkbox"
+              onChange={function () {
+                toggleDone(el.item);
+              }}
+            />
+            <div className={el.done ? "strike" : ""}>{el.item}</div>
+            <button
+              onClick={() => {
+                removeItem(el.item);
+              }}
+            >
+              X
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function Todo() {
+  // [ 'learn react' ]
+  // [ { item: 'learn react', done: false } ]
+  let [todoItems, setTodoItems] = useState([]);
+
+  function removeItem(content) {
+    const newArray = todoItems.filter(function (el) {
+      if (el.item === content) {
+        // don't include in the new array
+        return false;
+      }
+      return true;
+    });
+
+    setTodoItems(newArray);
+  }
+
+  function toggleDone(content) {
+    const newArray = todoItems.map(function (el) {
+      if (el.item === content) {
+        el.done = !el.done;
+      }
+      return el;
+    });
+    console.log(newArray);
+    setTodoItems(newArray);
+  }
+
+  return (
+    <>
+      <h2>What do you want to do today?</h2>
+      <TodoInput
+        onSubmit={function (newItem) {
+          // shortcut to copy and create a new reference
+          setTodoItems([...todoItems, { item: newItem, done: false }]);
+        }}
+      />
+      <TodoItem
+        toggleDone={toggleDone}
+        removeItem={removeItem}
+        items={todoItems}
+      />
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
+}
