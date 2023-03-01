47e7c2a3315b088a2ca9274339dd4e7b341e355d
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-03-01 23:08:56 +0530

    Update Next7Days.js

--- src/components/Next7Days.js
+++ src/components/Next7Days.js
@@ -1,25 +1,24 @@
 import React from "react";
 import ListRender from "./ListRender";
 
-const Today = (props) => {
+const Next7Days = (props) => {
+  const date = new Date();
   const filteredList = props.list.filter((task) => {
-    if (date.getFullYear() !== task.date.getFullYear()) {
+    const diffTime = Math.abs(task.date - date);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    if (diffDays < 8) {
+      return true;
+    } else {
       return false;
     }
-    if (date.getMonth() !== task.date.getMonth()) {
-      return false;
-    }
-    if (date.getDate() !== task.date.getDate()) {
-      return false;
-    }
-    return true;
   });
-  
+
+
   return (
-    <div id="today-list">
-      <ListRender list={filteredList} />
+    <div id="next-list">
+      <ListRender list={filteredList}/>
     </div>
   );
 };
 
-export default Today;
+export default Next7Days;
