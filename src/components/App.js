512a440d7562f18fab931a374902c26b3149dab8
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-03-05 23:00:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,28 @@
-import React, { useState } from "react";
-import SideNav from "./SideNav";
-import MainSection from "./MainSection";
-import Header from "./Header";
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../styles/App.css";
+import React, { useEffect, useState } from 'react'
+import SideNav from './SideNav';
+import MainSection from './MainSection';
+import Header from './Header';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../styles/App.css';
 const App = () => {
-  const [active, setActive] = useState("INBOX");
-
+  const [state, setState] = useState("INBOX");
+  function kroChange(inp){
+     setState(inp);
+  }
   return (
     <div id="main">
-      <Header />
-      <div className="row">
-        <div className="col-md-2">
-          <SideNav change={setActive} />
+    <Header />
+      <div className='row'>
+        <div className='col-md-2'>
+          <SideNav change={kroChange}/>
         </div>
-        <div className="col-md-10">
-          <MainSection active={active} />
+        <div className='col-md-10'>
+          <MainSection active = {state}/>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
+
 
 export default App;
