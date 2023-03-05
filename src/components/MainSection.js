9df354cd90a46fbcecef6879315862c41f857507
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-03-05 22:13:42 +0530

    Update MainSection.js

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -6,23 +6,23 @@ import Today from "./Today";
 const list = [
   { number: 1, title: "Let's complete this", date: new Date("9/27/2022") },
   { number: 2, title: "Should sleep at 9pm", date: new Date("9/28/2022") },
-  { number: 3, title: "Should complete react", date: new Date("10/5/2022") },
+  { number: 3, title: "Should complete react", date: new Date("10/5/2022") }
 ];
 
 const MainSection = (props) => {
-  const [filteredlist, setFilteredList] = useState(list);
+  const [filteredList, setFilteredList] = useState(list);
   const addToList = (obj) => {
     list.push(obj);
     setFilteredList(list);
   };
-  
+
   return (
     <div className="main-section">
       {props.active === "INBOX" && (
-        <Inbox list={filteredlist} append={addToList} />
+        <Inbox list={filteredList} append={addToList} />
       )}
-      {props.active === "TODAY" && <Today list={filteredlist} />}
-      {props.active === "NEXT" && <Next7Days list={filteredlist} />}
+      {props.active === "TODAY" && <Today list={filteredList} />}
+      {props.active === "NEXT" && <Next7Days list={filteredList} />}
     </div>
   );
 };
